ff441d965f6436cdedc0240e4cfb1952f7a68bce
RedCoder4 <bd5@hotmail.com.au>
2017-10-12 22:52:40 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
+var DiscordStreamer = require("discordstreamer");
+var stream = new DiscordStreamer("MzY2ODM4ODAyNjY2MjI1NjY1.DLzYQA.NAvhXUcRXzsOPJ0u6Qvuunkcv1Y", __dirname, {"vc": "Voice channel ID", "feed": "Feed text channel ID", "djs": ["Someone's Discord user ID"], "masterUsers": ["Your Discord user ID"]});
+stream.init();
 module.exports = require('./lib/Worker.js');
